dfafbb366df9207122aef22016159412c15e1793
lnden <258231674@qq.com>
2021-08-24 19:08:36 +0800

    feat: develop the return menu list data format

--- routes/menus.js
+++ routes/menus.js
@@ -8,6 +8,40 @@ const Menu = require('../models/menuSchema')
 
 router.prefix('/menu')
 
+// 菜单列表查询
+router.get('/list', async (ctx) => {
+  const { menuName, menuState } = ctx.request.query
+  const params = {}
+  if (menuName)params.menuName = menuName
+  if (menuState)params.menuState = menuState
+  const result = await Menu.find(params) || []
+  const permissionList = getTreeMenu(result, null, [])
+  ctx.body = util.success(permissionList)
+})
+
+// 递归拼接属性列表
+function getTreeMenu(menuList, id, list) {
+  for(let i = 0; i < menuList.length; i++) {
+    let item = menuList[i]
+    if (String(item.parentId.slice().pop()) == String(id)) {
+      list.push(item._doc)
+    }
+  }
+  list.map(item => {
+     item.children = []
+     getTreeMenu(menuList, item._id, item.children)
+     if (item.children.length == 0) {
+       delete item.children
+     } else if (item.children[0].length > 0 && item.children[0].menuType == 2) {
+       // 快速区分按钮和菜单， 用于后期做菜单按钮权限控制
+       item.action = item.children
+      //  delete item.children
+     }
+  })
+  return list
+}
+
+// 菜单的增/删/改查功能
 router.post('/operate', async (ctx) => {
   const { _id, action, ...params } = ctx.request.body
   let res, info;
